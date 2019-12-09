4c4da3d5c21cd1aaceba06c3b0001b4f9ef704ab
Spencer Tuft <spencer.tuft@gmail.com>
2019-12-09 16:24:39 -0700

    Fix typo in complex object api type property

--- index.ts
+++ index.ts
@@ -50,14 +50,14 @@ export namespace UAPI {
 
   export interface ComplexObject<Options> {
     object: { [key: string]: Value | (Value & Options)}
-    api_type: 'read_only' | 'related'
+    api_type: 'read-only' | 'related'
     display_label?: string
     related?: string
   }
 
   export interface ComplexObjectArray<Options> {
     object_array: { [key: string]: Value | (Value & Options)}[]
-    api_type: 'read_only' | 'related'
+    api_type: 'read-only' | 'related'
     display_label?: string
     related?: string
   }
