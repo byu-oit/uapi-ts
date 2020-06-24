ff57f4581f004ba7909c0b88498330b60d54c2eb
Spencer Tuft <spencer.tuft@gmail.com>
2020-06-24 16:40:45 -0600

    Correct and simplify types, Improve Tests
    Some of the types were incorrect and hard to use. These changes make these types easier to instantly use and easily modify if necessary.
    A Test was added that shows how the types could be used.

--- index.ts
+++ index.ts
@@ -1,7 +1,39 @@
 export namespace UAPI {
-  export interface Links {
-    [key: string]: Link
+  export namespace Response {
+    export interface Simple {
+      links: Links
+      metadata: Metadata.Simple
+    }
+    export interface Collection<T extends Simple> {
+      links: Links
+      metadata: Metadata.Collection
+      values: T[]
+    }
   }
+
+  export namespace Metadata {
+    export interface ValidationResponse<T extends number = number> {
+      code: T
+      message: string
+    }
+    export type ValidationInformation = string[]
+    export interface Cache {
+      date_time: string
+    }
+    export type Restricted = boolean
+    export interface Simple {
+      validation_response: ValidationResponse
+      validation_information?: ValidationInformation
+      cache?: Cache
+      restricted?: Restricted
+    }
+    export type CollectionSize = number
+    export interface Collection extends Simple {
+      collection_size?: CollectionSize
+    }
+  }
+
+  export type Links = Record<string, Link>
 
   export interface Link {
     method: string
@@ -9,31 +41,31 @@ export namespace UAPI {
     href: string
   }
 
-  export interface CollectionMetadata extends SimpleMetadata {
-    collection_size?: number
+  export namespace Scalar {
+    export type Type = string | number | boolean | null
+    export type String = Value<string>
+    export type Number = Value<number>
+    export type Boolean = Value<boolean>
   }
 
-  export interface SimpleMetadata {
-    validation_response: ValidationResponse
-    validation_information?: string[]
-    cache?: {
-      date_time: string
-    }
-    restricted?: boolean
+  export type Property<T extends Scalar.Type> = Value<T> | ValueArray<T>
+
+  export type PropertyDictionary = Record<string, Property<any> | ComplexProperty<any>> | null
+
+  export type ComplexProperty<T extends PropertyDictionary> = Object<T> | ObjectArray<T>
+
+  export enum ApiType {
+    READONLY = 'read-only',
+    MODIFIABLE = 'modifiable',
+    SYSTEM = 'system',
+    DERIVED = 'derived',
+    UNAUTHORIZED = 'unauthorized',
+    RELATED = 'related'
   }
 
-  export interface ValidationResponse {
-    code: number
-    message: string
-  }
-
-  export type DefaultType = string | null
-
-  export interface DefaultOptions {}
-
-  export interface Value<T = DefaultType> {
-    value: T
-    api_type: 'read-only' | 'modifiable' | 'system' | 'derived' | 'unauthorized' | 'related'
+  export interface Value<V extends Scalar.Type> {
+    value: V | null
+    api_type: ApiType.READONLY | ApiType.MODIFIABLE | ApiType.SYSTEM | ApiType.DERIVED | ApiType.UNAUTHORIZED | ApiType.RELATED
     key?: boolean
     description?: string
     long_description?: string
@@ -42,9 +74,9 @@ export namespace UAPI {
     related_resource?: string
   }
 
-  export interface ValueArray<T = DefaultType, Options = DefaultOptions> {
-    value_array: Array<Value<T> | (Value<T> & Options)>
-    api_type: 'read-only' | 'modifiable' | 'system' | 'derived' | 'unauthorized' | 'related'
+  export interface ValueArray<T extends Scalar.Type> {
+    value_array: Value<T>[]
+    api_type: ApiType.READONLY | ApiType.MODIFIABLE | ApiType.SYSTEM | ApiType.DERIVED | ApiType.UNAUTHORIZED | ApiType.RELATED
     description?: string
     long_description?: string
     display_label?: string
@@ -52,16 +84,16 @@ export namespace UAPI {
     related_resource?: string
   }
 
-  export interface ComplexObject<T = DefaultType, Options = DefaultOptions> {
-    object: { [key: string]: Value<T> | (Value<T> & Options)}
-    api_type: 'read-only' | 'related'
+  export interface Object<T extends PropertyDictionary> {
+    object: T | null
+    api_type: ApiType.READONLY | ApiType.RELATED
     display_label?: string
     related_resource?: string
   }
 
-  export interface ComplexObjectArray<T = DefaultType, Options = DefaultOptions> {
-    object_array: { [key: string]: Value<T> | (Value<T> & Options)}[]
-    api_type: 'read-only' | 'related'
+  export interface ObjectArray<T extends PropertyDictionary> {
+    object_array: T[]
+    api_type: ApiType.READONLY | ApiType.RELATED
     display_label?: string
     related_resource?: string
   }
