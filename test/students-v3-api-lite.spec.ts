7d993f27e241aa66b6a2c1b826b8400e9c67212e
Spencer Tuft <stuft2@byu.edu>
2021-06-28 10:14:59 -0600

    Add UAPI Lite support in separate namespace

--- test/students-v3-api-lite.spec.ts
+++ test/students-v3-api-lite.spec.ts
@@ -1,52 +1,52 @@
 import { UAPI } from '../index'
 
 export namespace StudentsV3API {
-    export type Response = UAPI.Response.Collection<WaitlistedClass>
+    export type Response = UAPI.Lite.Response.Collection<WaitlistedClass>
 
     export type WaitlistedClass = {
-        byu_id: UAPI.Scalar.Type
-        year_term: UAPI.Scalar.Type
-        curriculum_id: UAPI.Scalar.Type
-        title_code: UAPI.Scalar.Type
-        teaching_area: UAPI.Scalar.Type
-        course_number: UAPI.Scalar.Type
-        section_number: UAPI.Scalar.Type
-        term_code: UAPI.Scalar.Type
-        class_provider: UAPI.Scalar.Type
-        instruction_mode: UAPI.Scalar.Type
-        percent_classroom_time: UAPI.Scalar.Type
-        course_title: UAPI.Scalar.Type
-        credit_hours: UAPI.Scalar.Type
-        end_date: UAPI.Scalar.Type
-        final_exam_schedule: UAPI.Object<RoomDetails>
-        help_section: UAPI.Scalar.Type
-        honors_section: UAPI.Scalar.Type
-        instructors: UAPI.ObjectArray<Instructor>
-        lab_section: UAPI.Scalar.Type
-        lms_name: UAPI.Scalar.Type
-        online_section: UAPI.Scalar.Type
-        position_in_queue: UAPI.Scalar.Type
-        quiz_section: UAPI.Scalar.Type
-        reason_skipped: UAPI.Scalar.Type
-        requested_by_byu_id: UAPI.Scalar.Type
-        requested_datetime: UAPI.Scalar.Type
-        start_date: UAPI.Scalar.Type
-        when_taught: UAPI.ObjectArray<RoomDetails>
+        byu_id: UAPI.Lite.Scalar.String
+        year_term: UAPI.Lite.Scalar.String
+        curriculum_id: UAPI.Lite.Scalar.String
+        title_code: UAPI.Lite.Scalar.String
+        teaching_area: UAPI.Lite.Scalar.String
+        course_number: UAPI.Lite.Scalar.String
+        section_number: UAPI.Lite.Scalar.String
+        term_code: UAPI.Lite.Scalar.String
+        class_provider: UAPI.Lite.Scalar.String
+        instruction_mode: UAPI.Lite.Scalar.String
+        percent_classroom_time: UAPI.Lite.Scalar.String
+        course_title: UAPI.Lite.Scalar.String
+        credit_hours: UAPI.Lite.Scalar.String
+        end_date: UAPI.Lite.Scalar.String
+        final_exam_schedule: UAPI.Lite.Object<RoomDetails>
+        help_section: UAPI.Lite.Scalar.Boolean
+        honors_section: UAPI.Lite.Scalar.Boolean
+        instructors: UAPI.Lite.ObjectArray<Instructor>
+        lab_section: UAPI.Lite.Scalar.Boolean
+        lms_name: UAPI.Lite.Scalar.String
+        online_section: UAPI.Lite.Scalar.Boolean
+        position_in_queue: UAPI.Lite.Scalar.Number
+        quiz_section: UAPI.Lite.Scalar.Boolean
+        reason_skipped: UAPI.Lite.Scalar.String
+        requested_by_byu_id: UAPI.Lite.Scalar.String
+        requested_datetime: UAPI.Lite.Scalar.String
+        start_date: UAPI.Lite.Scalar.String
+        when_taught: UAPI.Lite.ObjectArray<RoomDetails>
     }
 
     export type RoomDetails = {
-        building: UAPI.Scalar.Type
-        room: UAPI.Scalar.Type
-        days: UAPI.Scalar.Type
-        start_time: UAPI.Scalar.Type
-        end_time: UAPI.Scalar.Type
-        start_date: UAPI.Scalar.Type
-        end_date: UAPI.Scalar.Type
+        building: UAPI.Lite.Scalar.String
+        room: UAPI.Lite.Scalar.String
+        days: UAPI.Lite.Scalar.String
+        start_time: UAPI.Lite.Scalar.String
+        end_time: UAPI.Lite.Scalar.String
+        start_date: UAPI.Lite.Scalar.String
+        end_date: UAPI.Lite.Scalar.String
     }
 
     export type Instructor = {
-        instructor_byu_id: UAPI.Scalar.Type
-        instructor_type: UAPI.Scalar.Type
+        instructor_byu_id: UAPI.Lite.Scalar.String
+        instructor_type: UAPI.Lite.Scalar.String
     }
 }
 
@@ -104,32 +104,26 @@ export namespace StudentsV3API {
                 credit_hours: '3.0',
                 end_date: '2018-12-13',
                 final_exam_schedule: {
-                    object: {
-                        building: 'RB',
-                        room: '164',
-                        days: 'S',
-                        start_time: '11:00',
-                        end_time: '14:00',
-                        start_date: '2018-12-15',
-                        end_date: '2018-12-15'
-                    },
-                    api_type: UAPI.ApiType.READONLY
+                    building: 'RB',
+                    room: '164',
+                    days: 'S',
+                    start_time: '11:00',
+                    end_time: '14:00',
+                    start_date: '2018-12-15',
+                    end_date: '2018-12-15'
                 },
                 help_section: false,
                 honors_section: false,
-                instructors: {
-                    object_array: [
-                        {
-                            instructor_byu_id: '333333333',
-                            instructor_type: 'PRIMARY'
-                        },
-                        {
-                            instructor_byu_id: '555555555',
-                            instructor_type: 'TEACHING ASSISTANT'
-                        }
-                    ],
-                    api_type: UAPI.ApiType.READONLY
-                },
+                instructors: [
+                    {
+                        instructor_byu_id: '333333333',
+                        instructor_type: 'PRIMARY'
+                    },
+                    {
+                        instructor_byu_id: '555555555',
+                        instructor_type: 'TEACHING ASSISTANT'
+                    }
+                ],
                 lab_section: false,
                 lms_name: 'Learning Suite',
                 online_section: false,
@@ -139,29 +133,26 @@ export namespace StudentsV3API {
                 requested_by_byu_id: '123456789',
                 requested_datetime: '2018-07-04T11:22:33.445Z',
                 start_date: '2018-09-04',
-                when_taught: {
-                    object_array: [
-                        {
-                            building: 'RB',
-                            room: '206',
-                            days: 'MW',
-                            start_time: '12:00',
-                            end_time: '12:50',
-                            start_date: '2018-09-04',
-                            end_date: '2018-12-13'
-                        },
-                        {
-                            building: 'RB',
-                            room: '164',
-                            days: 'F',
-                            start_time: '12:00',
-                            end_time: '12:50',
-                            start_date: '2018-09-04',
-                            end_date: '2018-12-13'
-                        }
-                    ],
-                    api_type: UAPI.ApiType.READONLY
-                }
+                when_taught: [
+                    {
+                        building: 'RB',
+                        room: '206',
+                        days: 'MW',
+                        start_time: '12:00',
+                        end_time: '12:50',
+                        start_date: '2018-09-04',
+                        end_date: '2018-12-13'
+                    },
+                    {
+                        building: 'RB',
+                        room: '164',
+                        days: 'F',
+                        start_time: '12:00',
+                        end_time: '12:50',
+                        start_date: '2018-09-04',
+                        end_date: '2018-12-13'
+                    }
+                ]
             }
         ]
     }
